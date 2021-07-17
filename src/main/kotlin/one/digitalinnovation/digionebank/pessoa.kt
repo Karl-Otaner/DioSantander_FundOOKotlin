67c5d77fa46d625874ae2bae971d48d8f80f1e83
karl-otaner <carlosrnsilva01@gmail.com>
2021-07-17 20:08:23 -0300

    Atualizando para falar sobre class menbers

--- src/main/kotlin/one/digitalinnovation/digionebank/pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/pessoa.kt
@@ -1,19 +1,20 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-    var name: String = "Carlos"
+    var nome: String = "Carlos"
     var cpf: String = "123.123.123-11"
     private set // deixando privado o set, não é + permitido atribuir valor a propriedade!
 
+    constructor()
+
+    fun pessoalinf() = "$nome e $cpf"
 }
 
 fun main() {
     val carlos = Pessoa()
 
-    println(carlos)
-    println(carlos.name)
-    println(carlos.cpf)
 
-   // println(carlos.Endereco().rua)
+    println(carlos.pessoalinf())
+
 
 }
