35d263b1d6d6091536c217a3982b6dfa80d2517d
karl-otaner <carlosrnsilva01@gmail.com>
2021-07-19 01:44:52 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/pessoa.kt
@@ -1,18 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Carlos"
-    var cpf: String = "123.123.123-11"
-    private set // deixando privado o set, não é + permitido atribuir valor a propriedade!
+abstract class Pessoa(
+     val nome: String,
+     val cpf: String
+)
 
-    constructor()
-
-    fun pessoalinf() = "$nome e $cpf"
-}
-
-fun main() {
-    val carlos = Pessoa()
-    println(carlos.pessoalinf())
-
-
-}
