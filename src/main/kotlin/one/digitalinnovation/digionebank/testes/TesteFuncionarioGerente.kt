1440dc2eb747c5a7149a8aca75ef0dcc5210163a
karl-otaner <carlosrnsilva01@gmail.com>
2021-07-20 22:37:52 -0300

    Fim as Aulas de POO Kotlin

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,6 +3,8 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main(){
-    val maria = Gerente( "Maria do Carmoo", "9987654321", 5000.0)
+    val maria = Gerente( "Maria do Carmoo", "9987654321", 5000.0, "senha123")
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
